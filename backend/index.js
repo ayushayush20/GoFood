f7dce3a9da1611d86a5e4aad0fad80ea00516849
Ayush Tyagi <ayush.tyagi@greenleaprobotics.com>
2024-01-16 16:35:14 +0530

    revert all

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express')
 
-const cors = require('cors')
+//const cors = require('cors')
 
 //const punycode = require('punycode');
 const app = express()
@@ -12,8 +12,8 @@ mongoDB();
 //app.use(cors())
 
 app.use((req, res, next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://13.232.106.215");
-  //res.setHeader("Access-Control-Allow-Origin","*");
+  //res.setHeader("Access-Control-Allow-Origin","http://13.232.106.215");
+  res.setHeader("Access-Control-Allow-Origin","*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
