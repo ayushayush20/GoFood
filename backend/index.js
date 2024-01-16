5a0eebccc000ae4394ab70d7e7684ac3203a2233
Ayush Tyagi <ayush.tyagi@greenleaprobotics.com>
2024-01-16 16:28:17 +0530

    cors allow server ip

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,8 @@ mongoDB();
 //app.use(cors())
 
 app.use((req, res, next)=>{
-  //res.setHeader("Access-Control-Allow-Origin","http://13.232.106.215");
-  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Origin","http://13.232.106.215");
+  //res.setHeader("Access-Control-Allow-Origin","*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
