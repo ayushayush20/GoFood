e78d39ebae4d760c1f612cef1f6ca71a1651e684
Ayush Tyagi <ayush.tyagi@greenleaprobotics.com>
2024-01-16 15:15:29 +0530

    used cors globally

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const express = require('express')
+
+const cors = require('cors')
+
 //const punycode = require('punycode');
 const app = express()
 const port = 5000;
 const mongoDB = require('./db')
 mongoDB();
 
+app.use(cors())
+/*
 app.use((req, res, next)=>{
   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
   //res.setHeader("Access-Control-Allow-Origin","*");
@@ -14,7 +19,7 @@ app.use((req, res, next)=>{
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-})
+})*/
 app.use(express.json());
 app.use('/api', require("./routes/CreateUser"));
 app.use('/api', require("./routes/DisplayData"));
