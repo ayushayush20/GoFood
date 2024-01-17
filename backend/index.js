fbfdf6de0838bd4bfebe3e41edf24615ecaa5b2a
Ayush Tyagi <ayush.tyagi@greenleaprobotics.com>
2024-01-17 21:05:07 +0530

    buildpath

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express')
-
+const path = require("path");
 //const cors = require('cors')
+
+//used for serving the build of react as static files 
+const buildPath = path.join(__dirname, "../build"); 
 
 //const punycode = require('punycode');
 const app = express()
@@ -10,6 +13,12 @@ const mongoDB = require('./db')
 mongoDB();
 
 //app.use(cors())
+
+app.use(express.static(buildPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+  });
 
 app.use((req, res, next)=>{
   //res.setHeader("Access-Control-Allow-Origin","http://13.232.106.215");
